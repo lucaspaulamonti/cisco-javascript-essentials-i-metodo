51ba2fa590af2e7793b2974141649cff7c0a9403
lucaspaulamonti <lucasdepaulamonti@gmail.com>
2022-10-18 00:09:42 -0300

    Create metodo.js

---
+++ metodo.js
@@ -0,0 +1,14 @@
+// Métodos lenght, charAt, slice e split.
+let str = "java script language";
+
+console.log(str.length);
+console.log('test'.length);
+
+console.log(str.charAt(0));
+console.log('abc'.charAt(1));
+
+console.log(str.slice(0, 4));
+console.log('test'.slice(1, 3));
+
+console.log(str.split(' '));
+console.log('192.168.1.1'.split('.'));
